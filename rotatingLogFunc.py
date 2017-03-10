fd5c4b1ad8486e6021190e0c3af7b305b322ee93
weightedEights <poormanjosh@gmail.com>
2017-03-10 14:51:23 -0300

    comments and structuring

--- rotatingLogFunc.py
+++ rotatingLogFunc.py
@@ -4,22 +4,43 @@
 from __future__ import print_function
 
 """
-Intro Text
+This script will write a timestamp to a time rotating log file. The purpose is to use this as a test bed for
+implementing the rotating log file handler in other projects.
 """
 
 from datetime import datetime
+import os
+import logging
 
 
 def main():
 
     printHeader()
 
+    # set the log file directory
+    logFileDir = os.getcwd()
+
+    logFile = logFileSetup(logFileDir)
+
+    rotatingLog(logFile)
+
+
 
 def printHeader():
     print("---------------------------------")
-    print("   Rotating Log Test Script")
+    print("    Rotating Log Test Script")
     print("   " + str(datetime.now()))
     print("---------------------------------")
+
+
+def logFileSetup(fileDir):
+
+    pass
+
+
+def rotatingLog(logFile):
+
+    pass
 
 
 if __name__ == '__main__':
